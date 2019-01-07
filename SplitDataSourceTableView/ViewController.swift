2027cbf29a5c48311457d58614a836c8953708a4
yodaaa <01ewell0@gmail.com>
2019-01-07 15:53:44 +0900

    Initial Commit

---
+++ SplitDataSourceTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitDataSourceTableView
+//
+//  Created by yodaaa on 2019/01/07.
+//  Copyright © 2019 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SplitDataSourceTableViewTests/SplitDataSourceTableViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  SplitDataSourceTableViewTests.swift
+//  SplitDataSourceTableViewTests
+//
+//  Created by yodaaa on 2019/01/07.
+//  Copyright © 2019 yodaaa. All rights reserved.
+//
+
+import XCTest
+@testable import SplitDataSourceTableView
+
+class SplitDataSourceTableViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
