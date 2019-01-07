cbf73b118b705905c38cc1bd8d0ece65c75e6c7f
yodaaa <01ewell0@gmail.com>
2019-01-07 18:17:57 +0900

    tableviewのdelegateとdatasourceを切り分け

---
+++ SplitDataSourceTableView/TableViewDataSourceDelegateController.swift
@@ -0,0 +1,62 @@
+//
+//  TableViewDataSourceDelegateViewController.swift
+//  SplitDataSourceTableView
+//
+//  Created by yodaaa on 2019/01/07.
+//  Copyright © 2019 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class TableViewDataSourceDelegateController: UITableView, UITableViewDelegate, UITableViewDataSource {
+
+    
+    var tableData: [[String]] = [
+        ["a", "b", "c"],
+        ["d", "e"],
+        ["f"],
+        ["g", "h", "i", "j", "k"],
+    ]
+    
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+    func numberOfSections(in tableView: UITableView) -> Int { // sectionの数を決める
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+
+        cell.textLabel?.text = tableData[indexPath.section][indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("didSelectRowAt: \(indexPath)")
+        
+        // タップ後すぐ非選択状態にするには下記メソッドを呼び出します．
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    
+    
+    
+    
+   
+
+}
--- SplitDataSourceTableView/ViewController.swift
+++ SplitDataSourceTableView/ViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let tableView = UINib(nibName: "View", bundle: nil).instantiate(withOwner: self, options: nil).first as! UITableView
+
+    var tableViewDataSourceDelegate: TableViewDataSourceDelegateController = TableViewDataSourceDelegateController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        tableView.register(UINib(nibName: "tableViewCell", bundle: nil), forCellReuseIdentifier: "tableViewCell")
+        
+        tableView.delegate = tableViewDataSourceDelegate
+        tableView.dataSource = tableViewDataSourceDelegate
+        
+        self.view.addSubview(tableView)
     }
     
 
